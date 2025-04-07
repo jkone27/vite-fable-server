98e2556c16c95436ca7caa606b270a0f1a39dcfa
gparmigiani <gparmigiani@travix.com>
2025-04-07 17:11:08 +0200

    first commit not working

---
+++ src/App.fs
@@ -0,0 +1,18 @@
+﻿open Fable.Core
+open Glutinum.Express
+open Glutinum.ExpressServeStaticCore
+
+let port = 3000
+let app = express.express ()
+
+app.get (
+    "/",
+    fun (req: Request) (res: Response) ->
+        JS.console.log ($"New request, %s{req.hostname}")
+        res.send ("Hello world")
+)
+
+let viteNodeApp = app.listen (
+    port,
+    fun () -> JS.console.log $"Started listening on http://127.0.0.1:%i{port}"
+)
